21bb38ceb98c0a0b86cb278022b56b2b998726f2
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-09-07 08:46:18 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = "ap-south-1"
 }
-resource "aws_security_group" "ec2-sg" {
-  name = "ec2-sg"
+resource "aws_security_group" "ec2web2-sg" {
+  name = "ec2web2-sg"
   ingress {
    from_port   = 22
    to_port     = 22
@@ -25,14 +25,14 @@ resource "aws_security_group" "ec2-sg" {
   }
 
   tags ={
-    type ="terraform-test-security-group"
+    type ="terraform-test-security-group2"
   }
 }
 
 resource "aws_instance" "web2" {
   ami = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.ec2-sg.name]
+  security_groups = [aws_security_group.ec2web2-sg.name]
   tags = {
     Name = "web2"
   }
