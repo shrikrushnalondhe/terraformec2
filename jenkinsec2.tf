57888dd081594e74ac67b2a500d9f1c1dbae7f46
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-09-07 17:09:31 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -36,4 +36,16 @@ resource "aws_instance" "web2" {
   tags = {
     Name = "web2"
   }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "jenkins"
+      private_key = file("/home/jenkins/aws_key")
+      timeout     = "4m"
+   }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxl0BZRxinbQQm45+pKn47SWDYKjCvP0TTslq8E7vL2uZ/VEC0hm4c4Fe6+qMJhl8tg2tt5JNG6PY0d1yWgZMu47QpVciYfYNJLSvGGlXknFVU6FXlNRQEmuZqNWqemJZq5qEHsUGjwwoS2wqL9Z3kih0QdnxhjFpWJYVvc85mpN7sqqLAeawNEY2v31n/AuVvJ785/BaxWsvAaFTBa0U0R5I/asurhgphuHovcTCP7I335t4FF1UhS0OHFJQi2CxUxuKq/8KE6QHTZ27+YSNZ092BMOHWGehhFwvzBYIkHTszjMS8eEmuIekia4DJoyJ/luMLQ+KhxPGWhYQlXohL jenkins@ip-172-31-36-82.ap-south-1.compute.internal"
 }
