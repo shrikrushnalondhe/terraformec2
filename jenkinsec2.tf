a0915f2954dac7206a9f87c9a9ee2ce4d9f3e073
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-09-07 18:33:57 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -32,6 +32,7 @@ resource "aws_security_group" "ec2web2-sg" {
 resource "aws_instance" "web2" {
   ami = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
+  key_name= "aws_key"
   security_groups = [aws_security_group.ec2web2-sg.name]
   tags = {
     Name = "web2"
@@ -40,12 +41,12 @@ resource "aws_instance" "web2" {
       type        = "ssh"
       host        = self.public_ip
       user        = "jenkins"
-      private_key = file("/home/jenkins/aws_key")
+      private_key = file("/home/jenkins/keys/aws_key")
       timeout     = "4m"
    }
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxl0BZRxinbQQm45+pKn47SWDYKjCvP0TTslq8E7vL2uZ/VEC0hm4c4Fe6+qMJhl8tg2tt5JNG6PY0d1yWgZMu47QpVciYfYNJLSvGGlXknFVU6FXlNRQEmuZqNWqemJZq5qEHsUGjwwoS2wqL9Z3kih0QdnxhjFpWJYVvc85mpN7sqqLAeawNEY2v31n/AuVvJ785/BaxWsvAaFTBa0U0R5I/asurhgphuHovcTCP7I335t4FF1UhS0OHFJQi2CxUxuKq/8KE6QHTZ27+YSNZ092BMOHWGehhFwvzBYIkHTszjMS8eEmuIekia4DJoyJ/luMLQ+KhxPGWhYQlXohL jenkins@ip-172-31-36-82.ap-south-1.compute.internal"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7sdlkeQmUlFovLFrPGJuL+DyqcZzlqmtdCFY390lUjTvSqUVqLpgeJ0xFg+d8RD6LU+lPnv1rEBED4ax2FcCgumSmw1LYj+xEOQik+MxeJed1oRX2sHgMRy2IloFnyg+g6oBt8q30AhVy4eN+3qykhDQxYflpi3enVJgu5oD5hBJlfrLtj7Zr6hDlWQnCFkJ6PvwijhVYC6Sr3UIdMP9bA9DLxrkxkUHR/gzbFqdO3iWtDAgODG/gKRB8zhPl093dXGeZx85aG0sjCYt4UUqRdXV6EtDJPMxwN6NPP8uWbbCsz5tH0q1AcJwyVlvr5QfmgrF9Zx2zxC5Ts21lu3CX jenkins@ip-172-31-39-222.ap-south-1.compute.internal"
 }
