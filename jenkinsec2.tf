ae44d6e846cb0aa0dd8560ae91773d80e164cb28
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-02 18:36:42 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = "ap-south-1"
 }
-resource "aws_security_group" "ec2web2-sg" {
-  name = "ec2web2-sg"
+resource "aws_security_group" "ec2docker-sg" {
+  name = "ec2docker-sg"
   ingress {
    from_port   = 22
    to_port     = 22
@@ -17,6 +17,13 @@ resource "aws_security_group" "ec2web2-sg" {
    cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
    from_port   = 0
    to_port     = 0
@@ -29,13 +36,13 @@ resource "aws_security_group" "ec2web2-sg" {
   }
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "dockerec2" {
   ami = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
   key_name= "aws_key"
-  security_groups = [aws_security_group.ec2web2-sg.name]
+  security_groups = [aws_security_group.ec2docker-sg.name]
   tags = {
-    Name = "web2"
+    Name = "dockerec2"
   }
   connection {
       type        = "ssh"
@@ -48,8 +55,8 @@ resource "aws_instance" "web2" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7sdlkeQmUlFovLFrPGJuL+DyqcZzlqmtdCFY390lUjTvSqUVqLpgeJ0xFg+d8RD6LU+lPnv1rEBED4ax2FcCgumSmw1LYj+xEOQik+MxeJed1oRX2sHgMRy2IloFnyg+g6oBt8q30AhVy4eN+3qykhDQxYflpi3enVJgu5oD5hBJlfrLtj7Zr6hDlWQnCFkJ6PvwijhVYC6Sr3UIdMP9bA9DLxrkxkUHR/gzbFqdO3iWtDAgODG/gKRB8zhPl093dXGeZx85aG0sjCYt4UUqRdXV6EtDJPMxwN6NPP8uWbbCsz5tH0q1AcJwyVlvr5QfmgrF9Zx2zxC5Ts21lu3CX jenkins@ip-172-31-39-222.ap-south-1.compute.internal"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5fmqkSwm2QrQjZ49LxlnpASPxGJnPW0IJ0BIkhakO77zDV3R6BZFNn5rpNUijl3SCIVRdl2oh8lta66KdVNFAxskntohO/xrflEjD7ki0vy/ogldwK/Te9MvtMhtmZgbHGXCscsCaCmDL6eH3F4CNnZVX0/fhsRl1BnRgrGfH3m2JxvJrqqn/o46HmJMNRKFs1WIP0US9V2lcKJdQ0Qyx7pEn17uHTGgDADElE9ZjY/Wx+mzQwGBdN5xPN3vS++/+rLe/uMNVNATUtzBmMxkLLB1avCZrtBLM8Uib7/XjX0TVfPDuPlwAdH+K1bskrj/WFKA0kA4uY1Th0XgmQip5 root@ip-172-31-11-83.ap-south-1.compute.internal"
 }
 output "instance_ips" {
-  value = aws_instance.web2.public_ip
+  value = aws_instance.dockerec2.public_ip
 }
