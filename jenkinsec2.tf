f2a09fa3a743faffed6564331acdc9830e10cb47
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-03 13:04:22 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -57,4 +57,6 @@ resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5fmqkSwm2QrQjZ49LxlnpASPxGJnPW0IJ0BIkhakO77zDV3R6BZFNn5rpNUijl3SCIVRdl2oh8lta66KdVNFAxskntohO/xrflEjD7ki0vy/ogldwK/Te9MvtMhtmZgbHGXCscsCaCmDL6eH3F4CNnZVX0/fhsRl1BnRgrGfH3m2JxvJrqqn/o46HmJMNRKFs1WIP0US9V2lcKJdQ0Qyx7pEn17uHTGgDADElE9ZjY/Wx+mzQwGBdN5xPN3vS++/+rLe/uMNVNATUtzBmMxkLLB1avCZrtBLM8Uib7/XjX0TVfPDuPlwAdH+K1bskrj/WFKA0kA4uY1Th0XgmQip5 root@ip-172-31-11-83.ap-south-1.compute.internal"
 }
-
+output "instance_ips" {
+  value = aws_instance.dockerec2.public_ip
+}
