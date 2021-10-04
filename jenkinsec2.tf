93551d6a8c9884892e8cba7192c34549e0804dce
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-04 16:32:56 +0530

    Update jenkinsec2.tf

--- jenkinsec2.tf
+++ jenkinsec2.tf
@@ -36,13 +36,13 @@ resource "aws_security_group" "ec2docker-sg" {
   }
 }
 
-resource "aws_instance" "dockerec2" {
+resource "aws_instance" "dockerec21" {
   ami = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
   key_name= "aws_key"
   security_groups = [aws_security_group.ec2docker-sg.name]
   tags = {
-    Name = "dockerec2"
+    Name = "dockerec21"
   }
   connection {
       type        = "ssh"
@@ -58,5 +58,5 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5fmqkSwm2QrQjZ49LxlnpASPxGJnPW0IJ0BIkhakO77zDV3R6BZFNn5rpNUijl3SCIVRdl2oh8lta66KdVNFAxskntohO/xrflEjD7ki0vy/ogldwK/Te9MvtMhtmZgbHGXCscsCaCmDL6eH3F4CNnZVX0/fhsRl1BnRgrGfH3m2JxvJrqqn/o46HmJMNRKFs1WIP0US9V2lcKJdQ0Qyx7pEn17uHTGgDADElE9ZjY/Wx+mzQwGBdN5xPN3vS++/+rLe/uMNVNATUtzBmMxkLLB1avCZrtBLM8Uib7/XjX0TVfPDuPlwAdH+K1bskrj/WFKA0kA4uY1Th0XgmQip5 root@ip-172-31-11-83.ap-south-1.compute.internal"
 }
 output "instance_ips" {
-  value = aws_instance.dockerec2.public_ip
+  value = aws_instance.dockerec21.public_ip
 }
